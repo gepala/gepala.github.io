0191803982a2fe84383971977a8f8d6372024bfd
gepala <ppgpr.gepala@gmail.com>
2025-07-28 18:17:31 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/gepala.github.io",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
